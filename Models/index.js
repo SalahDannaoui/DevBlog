b8bd130d7d4cc3cd987d90c9a2a494a4a173d914
Salah <dannaouisalah@gmail.com>
2023-06-06 12:47:54 +1000

    more file structuring, added, helpers.js in utils, created models, filled index.js, set up views file structure, set up server.js

---
+++ Models/index.js
@@ -0,0 +1,43 @@
+const Blog = require('./blog');
+const User = require('./user');
+const Comment = require('./comment');
+
+// A user can have many blog posts
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+// Blog belongs to user
+Blog.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+// A blog post can have many comments
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id',
+    onDelete: 'CASCADE'
+});
+
+// comment is connected to one blog post
+Comment.belongsTo(BlogPost, {
+    foreignKey: 'blog_id'
+});
+
+// a user can have many comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+// a comment only has one user
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = {
+    Blog,
+    User,
+    Comment
+};
